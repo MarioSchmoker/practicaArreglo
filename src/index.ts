03d694cef3d5279c551b99db0c393ee71d87b107
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-05-24 08:04:29 -0300

    cambio

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ let indice: number = 0;
 for (indice = 0; indice < 5; indice++) {
   numero = Number(prompt("Ingrese un Número"));
   num[indice] = numero;
-  console.log("el número en la posición", indice, " es ", num[indice]);
 }
-
-// while (indice < 5) {
-//   indice++;
-// }
+for (indice = 0; indice < 5; indice++) {
+  num[indice] = numero;
+  // console.log("el número en la posición", indice, " es ", num[indice]);
+  console.log("El número en la posición", indice, "es ", num[indice]);
+}
